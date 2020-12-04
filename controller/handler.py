689dd51d2be85a9114a9ff7d3ec823d9a89197b2
sergab11 <75457521+sergab11@users.noreply.github.com>
2020-12-04 00:42:20 -0300

    Update handler.py

--- controller/handler.py
+++ controller/handler.py
@@ -1,19 +1,55 @@
 from model import ludo
 from view  import des_canvas
+from tkinter import filedialog
 from tkinter import messagebox
-import json
-
 
 ladoCasa = 40
+
+def selecionaArq():
+	input = filedialog.askopenfile(initialdir=".", title="Super Ludo", filetypes=(("text files","*.txt"),("all files","*.*")))
+	if input == None:
+		messagebox.showerror("Erro", "Erro ao carregar arquivo!")
+		return
+	carregar(input)
+
+def salvaArq():
+	global jogo
+
+	data = [("text files","*.txt"),("all files","*.*")]
+	file = filedialog.asksaveasfile(initialdir=".", title="Super Ludo", filetypes=data, defaultextension=data)
+	if file == None:
+		messagebox.showerror("Erro", "Erro ao salvar arquivo!")
+		return
+
+	ludo.salvaContexto(jogo, file)
 
 def novoJ():
 	global jogo 
 	jogo = ludo.novoJogo()
 	des_canvas.novo()
 
+def carregar(arquivo):
+	global jogo
+	jogo = ludo.novoJogo()
+	ludo.carregaContexto(jogo, arquivo)
+	des_canvas.aposclique()
+
 def getJogo():
 	global jogo
 	return jogo
+
+def escolheuDado(valor):
+	global jogo, valorDado
+	valorDado = valor
+	des_canvas.jogou(valorDado)
+
+	if ludo.casaInicialCheia(jogo['tabuleiro'], jogo['jogadorVez']):
+		if valorDado != 5:
+			ludo.proximo(jogo)
+			des_canvas.aposclique()
+
+	if valorDado == 5:
+		trata5()
 
 def lanca():
 	global jogo, valorDado
@@ -56,35 +92,13 @@ def escolhePeca(tabuleiro, jogadorVez, coordPonteiro):
 
 def mouseClica(event):
 	global valorDado, jogo
-	cores = ["red", "green", "yellow", "blue"]
-	print([event.x, event.y])
+
 	pecaEscolhida = escolhePeca(jogo['tabuleiro'], jogo['jogadorVez'], [event.x, event.y])
-	print(pecaEscolhida)
 	if pecaEscolhida == -1:
 		return
-	
-	print(jogo['jogadorVez'], pecaEscolhida, valorDado, ":", ludo.podeMoverPeca(jogo['tabuleiro'], jogo['jogadorVez'], pecaEscolhida, valorDado))
+		
 	if ludo.podeMoverPeca(jogo['tabuleiro'], jogo['jogadorVez'], pecaEscolhida, valorDado):
 		ludo.moverPeca(jogo, pecaEscolhida, valorDado, True)
 		des_canvas.aposclique()
-		if jogo['tabuleiro'][jogo['jogadorVez']].count(58) == 4:
-			colocacao = list(range(4))
-			colocacao.sort(key=lambda x: sum(jogo['tabuleiro'][x]), reverse=True)
-			print(colocacao)
-			messagebox.showinfo(message=str([cores[i] + ":  " + str(sum(jogo['tabuleiro'][i])) for i in colocacao]))
 	 
 	return
-
-def salvarJogo():
-	global jogo
-	f = open("jogoSalvo.txt", "w")
-	f.write(str(jogo))
-	f.close()
-
-def carregarJogo():
-	global jogo
-	f = open("jogoSalvo.txt", "r")
-	jogo = json.loads(f.read().replace("'", "\"")) 
-	f.close()
-	print(jogo['tabuleiro'])
-	des_canvas.novo()
