cba4625e0192adcf89592c25b13e4be1bf05e416
sergab11 <75457521+sergab11@users.noreply.github.com>
2020-12-04 00:40:31 -0300

    Update mainludo.py

--- mainludo.py
+++ mainludo.py
@@ -1,41 +1,56 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
+
 from view import des_canvas
 from model import ludo
 from controller import handler
 
-raiz = tkinter.Tk()
+def getValor():
+	v = int(escolhevalor.get())
+	escolhevalor.set("")
+	handler.escolheuDado(v)
+
+raiz = tk.Tk()
 raiz.title("Super Ludo")
 raiz.geometry('850x600')
 
-#criação do canvas tabuleiro na janela principal
-tab = tkinter.Frame(raiz, width=600, height=600)
+tab = tk.Frame(raiz, width=600, height=600)
 tab.pack(side="left", fill='y')
-cnv = tkinter.Canvas(tab, height=600, width=600)
+cnv = tk.Canvas(tab, height=600, width=600)
 cnv.bind("<Button-1>", handler.mouseClica)
 
-#criação dos botões das opções na janela
-botoes = tkinter.Frame(raiz)
+botoes = tk.Frame(raiz)
 botoes.pack(side="top", fill='y')
 
-njogo = tkinter.Button(botoes, text="Novo Jogo", command=handler.novoJ)
+njogo = tk.Button(botoes, text="Novo Jogo", command=handler.novoJ)
 njogo.pack(pady=10)
 
-carregar = tkinter.Button(botoes, text="Carregar Jogo", command=handler.carregarJogo)
+carregar = tk.Button(botoes, text="Carregar Jogo", command=lambda:handler.selecionaArq())
 carregar.pack(pady=10)
 
-salvar = tkinter.Button(botoes, text="Salvar", command=handler.salvarJogo)
-#salvar.config(state="disabled")
+salvar = tk.Button(botoes, text="Salvar", command=lambda:handler.salvaArq())
 salvar.pack(pady=10)
 
-texto = tkinter.Label(botoes, text="A JOGAR:")
-texto.config(font="Arial 16 bold", bd=2, height=1, width=18)
-texto.pack(pady=10)
+texto1 = tk.Label(botoes, text="A JOGAR:")
+texto1.config(font="Arial 16 bold", bd=2, height=1, width=18)
+texto1.pack(pady=10)
 
-cnvdado = tkinter.Canvas(botoes, height=50, width=50)
+cnvdado = tk.Canvas(botoes, height=50, width=50)
 cnvdado.pack(pady=10)
 
-lancar = tkinter.Button(botoes, text="Lançar Dado", command=handler.lanca)
-lancar.pack(pady=50)
+texto2 = tk.Label(botoes, text="Escolha o valor do dado:")
+texto2.config(font="Arial 12 bold", bd=2, height=1, width=30)
+texto2.pack(pady=5)
+
+escolhevalor = ttk.Combobox(botoes, values=["1", "2", "3", "4", "5", "6"])
+escolhevalor.pack(pady=5)
+
+enviavalor = tk.Button(botoes, text="Enviar", command=getValor)
+enviavalor.config(font="Arial 9 bold", bg="white", bd=2, height=1, width=7)
+enviavalor.pack(pady=3)
+
+lancar = tk.Button(botoes, text="Lançar Dado", command=handler.lanca)
+lancar.pack(pady=30)
 
 for i in (njogo, carregar, salvar, lancar):
 	i.config(font="Arial 12 bold", bg="white", bd=2, height=1, width=18)
